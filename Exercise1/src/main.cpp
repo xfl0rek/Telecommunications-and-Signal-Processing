a82b2ba5cdd0588d7af3bf689b9da068a1a399ea
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-03-24 16:44:36 +0100

    Menu changes.

--- Exercise1/src/main.cpp
+++ Exercise1/src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "../include/singleErrorCode.h"
 #include "../include/doubleErrorCode.h"
 #include "../include/algorithm.h"
 #include <fstream>
@@ -7,9 +6,9 @@
 int main() {
     int choice;
 
-    std::cout << "1. zakoduj" << std::endl;
-    std::cout << "2. odkoduj" << std::endl;
-
+    std::cout << "Choose an option:" << std::endl;
+    std::cout << "1. Encode the message." << std::endl;
+    std::cout << "2. Decode the message." << std::endl;
     std::cin >> choice;
 
     switch (choice) {
@@ -18,93 +17,11 @@ int main() {
             break;
         }
         case 2: {
-            std::fstream file("../message.txt");
-            if (!file.is_open()) {
-                std::cerr << "Failed to open the file." << std::endl;
-                break;
-            }
-
-            std::string message;
-            if (!std::getline(file, message)) {
-                std::cerr << "Failed to read the message." << std::endl;
-                file.close();
-                break;
-            }
-
-            file.close();
-
-            std::cout << "Poprawna wiadomosc: " << message << std::endl;
-
-            std::vector<bool> binary = algorithm::textToBinary(message, doubleErrorMatrix);
-
-            std::cout << "Poprawna wiadomosc na bity: ";
-            for (const bool v : binary) {
-                std::cout << v;
-            }
-            std::cout << std::endl;
-
-            std::vector<bool> codedMessage;
-            std::fstream codedFile("../codedMessage.txt");
-            if (!codedFile.is_open()) {
-                std::cerr << "Failed to open the file." << std::endl;
-                break;
-            }
-
-            char value;
-            while (codedFile >> value) {
-                if (value == '0') {
-                    codedMessage.push_back(0);
-                } else if (value == '1') {
-                    codedMessage.push_back(1);
-                } else {
-                    std::cerr << "Invalid value in the file." << std::endl;
-                    break;
-                }
-            }
-            codedFile.close();
-
-            std::cout << "Zakodowana wiadomosc: ";
-            for (const bool v : codedMessage) {
-                std::cout << v;
-            }
-            std::cout << std::endl;
-
-            std::cout << "Wektor bledu: ";
-            std::vector<bool> E = algorithm::getErrorVector(codedMessage, doubleErrorMatrix);
-
-            for (const bool v : E) {
-                std::cout << v;
-            }
-            std::cout << std::endl;
-
-            std::vector<bool> correctedMessage = algorithm::detectAndCorrectErrors(codedMessage, doubleErrorMatrix);
-
-            std::cout << "Poprawiona wiadomosc: ";
-
-            for (const bool v : correctedMessage) {
-                std::cout << v;
-            }
-            std::cout << std::endl;
-
-            std::vector<bool> noParityBits = algorithm::removeParityBits(correctedMessage, doubleErrorMatrix);
-
-            std::cout << "Poprawiona wiadomosc bez bitow parzystosci: ";
-
-            for (const bool v : noParityBits) {
-                std::cout << v;
-            }
-            std::cout << std::endl;
-
-            std::string correct = algorithm::binaryToText(noParityBits, doubleErrorMatrix);
-
-            std::cout << "Poprawna wiadomosc: " << correct << std::endl;
-
             algorithm::restoringDataAfterTransmission(doubleErrorMatrix);
-
             break;
         }
         default:
-            std::cout << "Zly wybor.";
+            std::cout << "Wrong choice.";
     }
 
     return 0;
