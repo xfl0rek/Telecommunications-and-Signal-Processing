d2592a198afd1b63f5505be689f57cdd6844cdc0
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-30 15:11:34 +0200

    [Exercise2] Added port constructor implementation.

---
+++ Exercise2/cmake-build-debug/CMakeFiles/Exercise2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/PC/CLionProjects/dupa/Telecommunications-and-Signal-Processing/Exercise2/src/main.cpp" "CMakeFiles/Exercise2.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Exercise2.dir/src/main.cpp.obj.d"
+  "C:/Users/PC/CLionProjects/dupa/Telecommunications-and-Signal-Processing/Exercise2/src/port.cpp" "CMakeFiles/Exercise2.dir/src/port.cpp.obj" "gcc" "CMakeFiles/Exercise2.dir/src/port.cpp.obj.d"
+  "C:/Users/PC/CLionProjects/dupa/Telecommunications-and-Signal-Processing/Exercise2/src/receiver.cpp" "CMakeFiles/Exercise2.dir/src/receiver.cpp.obj" "gcc" "CMakeFiles/Exercise2.dir/src/receiver.cpp.obj.d"
+  "C:/Users/PC/CLionProjects/dupa/Telecommunications-and-Signal-Processing/Exercise2/src/transmitter.cpp" "CMakeFiles/Exercise2.dir/src/transmitter.cpp.obj" "gcc" "CMakeFiles/Exercise2.dir/src/transmitter.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Exercise2/src/port.cpp
+++ Exercise2/src/port.cpp
@@ -6,7 +6,34 @@ port::port() {
     openPort();
 
     if (handle != INVALID_HANDLE_VALUE) {
-        std::cout << "dupa" << std::endl;
+        setupPort.DCBlength = sizeof(setupPort);
+        GetCommState(handle, &setupPort);
+        setupPort.BaudRate = CBR_9600;
+        setupPort.Parity = NOPARITY;
+		setupPort.StopBits = ONESTOPBIT;
+		setupPort.ByteSize = 8;
+
+		setupPort.fParity = TRUE;
+		setupPort.fDtrControl = DTR_CONTROL_DISABLE;
+		setupPort.fRtsControl = RTS_CONTROL_DISABLE;
+		setupPort.fOutxCtsFlow = FALSE;
+		setupPort.fOutxDsrFlow = FALSE;
+		setupPort.fDsrSensitivity = FALSE;
+		setupPort.fAbortOnError = FALSE;
+		setupPort.fOutX = FALSE;
+		setupPort.fInX = FALSE;
+		setupPort.fErrorChar = FALSE;
+		setupPort.fNull = FALSE;
+
+		timeouts.ReadIntervalTimeout = 10000;
+		timeouts.ReadTotalTimeoutMultiplier = 10000;
+		timeouts.ReadTotalTimeoutConstant = 10000;
+		timeouts.WriteTotalTimeoutMultiplier = 100;
+		timeouts.WriteTotalTimeoutConstant = 100;
+
+		SetCommState(handle, &setupPort);
+		SetCommTimeouts(handle, &timeouts);
+		ClearCommError(handle, &error, &portResources);
     } else {
         std::cout << "Connection failed. Check if the port exists or is not already occupied." << std::endl;
         exit(1);
@@ -16,7 +43,8 @@ port::port() {
 port::~port() = default;
 
 void port::openPort() {
-    handle = CreateFile(portName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    handle = CreateFile(portName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL,
+                        OPEN_EXISTING, 0, NULL);
 
     if (handle == INVALID_HANDLE_VALUE) {
         std::cerr << "Failed to open port. Error code: " << GetLastError() << std::endl;
